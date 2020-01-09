fcea8462ed59f9be9238feea2386ce56faf79458
olliekrk <krolik.olgierd@gmail.com>
2020-01-09 22:13:50 +0100

    Init project

---
+++ src/main/java/com/elemonated/networker/NetworkerApplication.java
@@ -0,0 +1,12 @@
+package com.elemonated.networker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/elemonated/networker/NetworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elemonated.networker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
