b4a7671abec4c659f3b0f9aacc69578c4ae1afe4
kbieniasz <kbieniasz@onet.eu>
2020-01-11 14:23:36 +0100

    getByID employee

--- src/main/java/com/elemonated/networker/service/EmployeeService.java
+++ src/main/java/com/elemonated/networker/service/EmployeeService.java
@@ -2,8 +2,11 @@ package com.elemonated.networker.service;
 
 import com.elemonated.networker.persistence.data.Employee;
 import com.elemonated.networker.persistence.repository.EmployeeRepository;
+import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -21,6 +24,10 @@ public class EmployeeService {
 
     public void deleteEmployee(long id) {
         employeeRepository.deleteById(id);
+    }
+
+    public Optional<Employee> getEmployeeById (Long id) {
+        return employeeRepository.findById(id);
     }
 
 }
